8c892b16b548a6ea021935f74b79485c86fe11e3
Pavel Tsypyshev <ptsypyshev@gmail.com>
2023-12-26 11:40:38 +0500

    Added some views and search handler

--- food_recipes_project/food_recipes_app/urls.py
+++ food_recipes_project/food_recipes_app/urls.py
@@ -1,15 +1,18 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from .views import Main, About, Register, Login, Logout, CreateRecipe, UpdateRecipe, ReadRecipe
+from . import views
 
 urlpatterns = [
-    path('', Main.as_view(), name='main'),    
-    path('About/', About.as_view(), name='about'),
-    path('login/', Login.as_view(), name='login'),
-    path('logout/', Logout.as_view(), name='logout'),
-    path('registration/', Register.as_view(), name='register'),
-    path('create/', CreateRecipe.as_view(), name='create_recipe'),
-    path('edit/<int:recipe_id>/', UpdateRecipe.as_view(), name='edit_recipe'),
-    path('recipe/<int:recipe_id>/', ReadRecipe.as_view(), name='read_recipe'),
+    path('', views.Main.as_view(), name='main'),    
+    path('about/', views.About.as_view(), name='about'),
+    path('faq/', views.Faq.as_view(), name='faq'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout'),
+    path('registration/', views.Register.as_view(), name='register'),
+    path('categories/', views.Categories.as_view(), name='categories'),
+    path('category/<int:category_id>/', views.CategoryView.as_view(), name='read_category'),
+    path('ingredients/', views.IngredientsView.as_view(), name='ingredients'),
+    path('create/', views.CreateRecipe.as_view(), name='create_recipe'),
+    path('edit/<int:recipe_id>/', views.UpdateRecipe.as_view(), name='edit_recipe'),
+    path('recipe/<int:recipe_id>/', views.ReadRecipe.as_view(), name='read_recipe'),
+    path('search/', views.Search.as_view(), name='search'),
 ]
